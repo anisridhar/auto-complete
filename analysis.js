a203e6395834e824a51c22615a91dd77005158fc
Ani Sridhar <asridha1@andrew.cmu.edu>
2015-11-28 22:57:00 -0500

    making sure file is ok

--- analysis.js
+++ analysis.js
@@ -4,7 +4,23 @@ var analysis = (function(){
 
     var fruits = [apple, orange, pear, peach, pineapple];
 
+    function addFruit(){
 
+        var fruitList = document.getElementById("fruitslist");
+        var option = document.createElement("option");
+        option.text = "pineapple";
+        fruitList.add(option);
+        return;
+    }
+
+    //METHODS
+
+    module.addFruit = function(){
+
+        addFruit();
+        return;
+
+    };
 
     return module;
 
